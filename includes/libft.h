405944ea8c4b3acf07ea1e561c81403d6ac3980a
Luis Roel <phaseon@Luiss-MacBook-Pro.local>
2020-02-03 05:22:19 -0500

    Bit bit more

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: luiroel <luiroel@studen.42.us.org>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/03 03:35:28 by luiroel           #+#    #+#             */
-/*   Updated: 2020/02/03 04:18:01 by luiroel          ###   ########.fr       */
+/*   Updated: 2020/02/03 05:20:16 by luiroel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,15 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_putendl(char const *s);
 int		ft_strlen(char *str);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
+void	ft_bzero(void *s, size_t n);
+int		ft_atoi(const char *str);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl_fd(char const *s, int fd);
 
 #endif
