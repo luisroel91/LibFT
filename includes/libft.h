5313d799f059df8fa3b8b463749e68a95e60c9a4
Luis Roel <phaseon@Luiss-MacBook-Pro.local>
2020-02-03 04:13:03 -0500

    Init

---
+++ includes/libft.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luiroel <luiroel@studen.42.us.org>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/03 03:35:28 by luiroel           #+#    #+#             */
+/*   Updated: 2020/02/03 04:11:27 by luiroel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct	s_list 	*next;
+}					t_list;
+
+int		ft_islowercase(int c);
+int		ft_isuppercase(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+void	ft_putchar(char c);
+int		ft_strlen(char *str);
+int		ft_toupper(int c);
+
+#endif
