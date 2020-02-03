ee5f755d36b97b2979be4b15f689266a27bcd301
Luis Roel <phaseon@Luiss-MacBook-Pro.local>
2020-02-03 04:27:07 -0500

    atoi

---
+++ srcs/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luiroel <luiroel@studen.42.us.org>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/03 04:18:57 by luiroel           #+#    #+#             */
+/*   Updated: 2020/02/03 04:26:11 by luiroel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+int		ft_atoi(const char *str)
+{
+	int i;
+	int sign;
+	int	result;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '-' || str[i] == '+')
+		{
+			if (str[i] == '-')
+			{
+				sign = -1;
+			}
+			i++;
+		}
+		if (ft_isdigit(str[i]))
+		{
+			result = result * 10 + (str[i] - '0');
+			i++;
+		}
+		else
+		{
+			i++;
+		}	
+	}
+	return (result * sign);
+}
